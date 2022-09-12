94fa1b7b791362066d6e3ee85c636e4f01a4640a
Mario Garcia <mario.ggar@gmail.com>
2022-09-13 01:08:03 +0200

    fix(ci): get data from yml

--- repositories.tf
+++ repositories.tf
@@ -3,6 +3,6 @@ module "branch-protection" {
   for_each           = local.configuration.repositories
   repository         = each.key
   branch             = each.value.branch-protection.branch
-  review_count       = 1
-  require_codeowners = true
+  review_count       = each.value.branch-protection.pull-requests.review-count
+  require_codeowners = each.value.branch-protection.pull-requests.codeowners
 }
